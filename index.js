3587fe5cbc7dbc4c4b02c285ea75416b4d368dee
*Felipe <*felipecarrapeiro@hotmail.com*>
2023-06-18 19:07:00 -0300

    updated index.js

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ function trataErro(erro) {
   throw new Error(chalk.red(erro.code, "não há arquivo no diretório"));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo) {
   const encoding = "utf-8";
-  fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-    if (erro) {
-      trataErro(erro);
-    }
-    console.log(chalk.green(texto));
-  });
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    extraiLinks(texto);
+  } catch (erro) {
+    trataErro(erro);
+  }
+}
+
+function extraiLinks(link) {
+  const regex = /\[([^\[^\]]*?)\]\((https?:\/\/[^\s\?#.]*.[^\s]*)\)/gm;
+  const capturas = [...link.matchAll(regex)];
+  const resultados = capturas.map((captura) => ({ [captura[1]]: captura[2] }));
+  console.log("resultados :>> ", resultados);
 }
 
 pegaArquivo("./arquivos/texto.md");
